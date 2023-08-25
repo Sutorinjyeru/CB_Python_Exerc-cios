83467828a4836059b7e3fc2920acf41ba9fe5d16
Sutorinjyeru <satoruhirou@gmail.com>
2023-08-25 01:47:13 -0300

    eita madruga boa

--- Revisão/ex3.py
+++ Revisão/ex3.py
@@ -5,3 +5,37 @@
 # que o usuário consulte a disponibilidade de
 # quartos, reserve um quarto específico e cancele uma reserva
 # Ao realizar uma reserva, o estado do quarto deve ser atualizado no dicionário.
+
+from random import choice
+
+rooms = {}
+
+for i in range(10):
+    rooms["Quarto " + str(i)] = choice([True, False])
+
+
+def main():
+
+    print_rooms()
+    while True:
+        x = input("Escolha o número do quarto desejado:\n")
+        rooms["Quarto " + x]
+        if rooms["Quarto " + x] == True:
+            rooms["Quarto " + x] = False
+            if input(f"{rooms} gostaria de cancelar a reserva?(1=Sim. 2=Não)\n") != "1":
+                rooms["Quarto " + x] = True
+                print_rooms()
+            else:
+                print("Muito obrigado. Volte sempre. ")
+                break
+        elif ["Quarto " + x] == False:
+            if input("Quarto indisponível, gostaria de reservar outro?(1=Sim. 2=Não)\n") != "1":
+                print("Tudo bem. Volte sempre! ")
+                break
+
+
+def print_rooms():
+    print(f"Disponibilidade dos quartos: {rooms}")
+
+
+main()
--- Revisão/ex4.py
+++ Revisão/ex4.py
@@ -1,12 +1,12 @@
 import sys
+
 import requests
 
-
 band = requests.get(
-"https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
+    "https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
 
 result = band.json()
 
 
-for c in result ["results"]:
+for c in result["results"]:
     print(c["trackName"])
