80c41e264dd34c8fb6446c427564b2c7b94e6158
Sutorinjyeru <satoruhirou@gmail.com>
2023-08-31 17:08:19 -0300

    fogo..

---
+++ Exercícios_Básicos_de_Classes_e_Encapsulamento/ex10.py
@@ -0,0 +1,25 @@
+# Crie uma classe Shape com um método abstrato calculate_area.
+# Em seguida, defina classes derivadas Circle e Triangle que herdam de Shape e 
+# implementam esse método para calcular suas respectivas áreas.
+
+class Shape:
+    def __init__(self, val, value):
+        self.val = val
+        self.value = value
+    
+    def calculate_area():
+        
+    
+class Circle(Shape):
+    def __init__(self):
+        super().__init__()
+    
+    #def calculate_area(raio):
+        #return 3.14 * (raio **2)
+    
+class Triangle(Shape):
+    def __init__(self):
+        super().__init__()
+
+    #def calculate_area(base, altura):
+        #return (base * altura) / 2
--- Exercícios_Básicos_de_Classes_e_Encapsulamento/ex9.py
+++ Exercícios_Básicos_de_Classes_e_Encapsulamento/ex9.py
@@ -3,4 +3,25 @@
 # fazer transferências entre contas e calcular o saldo total do banco.
 
 class Bank:
-    
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+    #def de criar novas contas
+    @classmethod
+    def get(cls):
+        saldo = int(input("Exemplo de saldo criado\n"))
+        return cls(saldo)
+
+    #def de transferência entre contas
+    def transfer(obj1, obj2, valor):
+        obj1.saldo -= valor
+        obj2.saldo += valor
+        print(obj1.saldo, obj2.saldo)
+
+    #calcular saldo total
+    def total(self, *accounts):
+        total = 0
+        for _ in range(len(accounts)+1):
+            total += self.saldo
+        return total
+
