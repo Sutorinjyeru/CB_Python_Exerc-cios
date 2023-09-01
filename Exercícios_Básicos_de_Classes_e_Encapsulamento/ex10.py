6ecb5f80b124d5381a3e61016119863ae9153b4e
Sutorinjyeru <satoruhirou@gmail.com>
2023-09-01 12:14:43 -0300

    .

--- Exercícios_Básicos_de_Classes_e_Encapsulamento/ex10.py
+++ Exercícios_Básicos_de_Classes_e_Encapsulamento/ex10.py
@@ -1,25 +1,27 @@
 # Crie uma classe Shape com um método abstrato calculate_area.
-# Em seguida, defina classes derivadas Circle e Triangle que herdam de Shape e 
+# Em seguida, defina classes derivadas Circle e Triangle que herdam de Shape e
 # implementam esse método para calcular suas respectivas áreas.
 
 class Shape:
     def __init__(self, val, value):
         self.val = val
         self.value = value
-    
+
     def calculate_area():
-        
-    
+        pass
+
+
 class Circle(Shape):
-    def __init__(self):
-        super().__init__()
-    
-    #def calculate_area(raio):
-        #return 3.14 * (raio **2)
-    
+    def __init__(self, val, value):
+        super().__init__(val, value)
+
+    # def calculate_area(raio):
+        # return 3.14 * (raio **2)
+
+
 class Triangle(Shape):
     def __init__(self):
         super().__init__()
 
-    #def calculate_area(base, altura):
-        #return (base * altura) / 2
+    # def calculate_area(base, altura):
+        # return (base * altura) / 2
