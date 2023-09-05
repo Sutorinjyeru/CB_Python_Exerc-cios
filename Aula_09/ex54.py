aba73e3d07dee78a66e22bc8174a7148c75fee40
Sutorinjyeru <satoruhirou@gmail.com>
2023-09-05 17:07:20 -0300

    commit dnv

--- Aula_09/ex54.py
+++ Aula_09/ex54.py
@@ -2,6 +2,6 @@
 # Escreva uma função que receba dois números como argumentos e retorne a soma desses números.
 
 def soma(x,y):
-    z = x + y
+    z = x / y
     return z
 
---
+++ Aula_09/teste.py
@@ -0,0 +1,4 @@
+from ex54 import soma
+
+assert soma(4,2) == 6
+assert soma(3,2) == 5
